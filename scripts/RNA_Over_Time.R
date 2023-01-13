a2b125155b6c644b8f3ce9cc3d07cd1839e8419b
Lexi Keene <lexi.h.keene@gmail.com>
2023-01-13 12:39:57 -0700

    fixed plots w/ updated qPCR data

--- scripts/RNA_Over_Time.R
+++ scripts/RNA_Over_Time.R
@@ -5,27 +5,26 @@ library(ggthemes)
 library(writexl)
 
 # read in data
-#fly <- read_xlsx("tidy_formats/fly_data.xlsx")
+fly <- read_xlsx("tidy_formats/fly_data2.xlsx")
 
-#mosquito <- read_xlsx("tidy_formats/mosquito_data.xlsx")
+mosquito <- read_xlsx("tidy_formats/mosquito_data2.xlsx")
 
 # fly data figures
 
 # pre-processing 
-#fly_data <- fly %>%
-#  filter(present == "y") %>% 
-#  type_convert() %>% 
-#  group_by(target, group, week, length) %>%
-#  mutate(mean_ct = mean(ct, na.rm = TRUE),
-#         sd_ct = sd(ct, na.rm = TRUE),
-#         sample_length = paste(target, length, sep = "_")) 
+fly_data2 <- fly %>%
+  filter(present == "y") %>% 
+  type_convert() %>% 
+  group_by(target, group, week, length) %>%
+  mutate(mean_ct = mean(ct, na.rm = TRUE),
+         sd_ct = sd(ct, na.rm = TRUE),
+         sample_length = paste(target, length, sep = "_")) 
 
 # determine delta ct, use average of time 2 week fresh as starting ct
-# calculate fold change by taking 2^-(delta_ct)
-#write_xlsx(fly_data, "tidy_formats/fly_data.xlsx")
+write_xlsx(fly_data2, "tidy_formats/fly_data3.xlsx")
 
 #read back in the data
-fly_data <- read_xlsx("tidy_formats/fly_data.xlsx")
+fly_data3 <- read_xlsx("tidy_formats/fly_data3.xlsx")
 
 hline_max1 <- data.frame(group = c("Dry", "Frozen"), target = c("La Jolla", "Nora", 
                                                                "Thika"), 
@@ -40,7 +39,8 @@ hline_min1 <- data.frame(group = c("Dry", "Frozen"), target = c("La Jolla", "Nor
 
 
 # Mean line bar of La Jolla, Nora & Thika targets
-ggplot(filter(fly_data, target %in% c("La Jolla", "Nora", "Thika"), group != "Fresh"), aes(x=week)) + 
+ggplot(filter(fly_data3, target %in% c("La Jolla", "Nora", "Thika"), 
+              group != "Fresh"), aes(x=week)) + 
   geom_hline(data = hline_max1, aes(yintercept = hline_1), alpha = 0.5) +
   geom_hline(data = hline_min1, aes(yintercept = hline_2), alpha = 0.5) +
   geom_point(aes(y=ct, fill=target), shape = 21, size = 1.5, stroke = 0.1, 
@@ -54,8 +54,8 @@ ggplot(filter(fly_data, target %in% c("La Jolla", "Nora", "Thika"), group != "Fr
   labs(x = "Time Since Sample Collection (weeks)", y = "Mean Ct", fill = "Target")
 
 
-
-ggsave("plots/Mean_LaJollaNoraThika.pdf", units = "in", width = 10, height = 8)
+# remove # to save plot
+#ggsave("plots/Mean_LaJollaNoraThika.pdf", units = "in", width = 10, height = 8)
 
 
 hline_max2 <- data.frame(group = c("Dry", "Dry", "Dry", "Dry", "Frozen", "Frozen",
@@ -74,7 +74,7 @@ hline_min2 <- data.frame(group = c("Dry", "Dry", "Dry", "Dry", "Frozen", "Frozen
                                     17.29496, NA, 17.70815, NA))
 
 # Mean line bar of galbut and RpL32 short and long
-ggplot(filter(fly_data, sample_length %in% c("Galbut_long", "Galbut_short", 
+ggplot(filter(fly_data3, sample_length %in% c("Galbut_long", "Galbut_short", 
                                              "RpL32_long", "RpL32_short"), 
               group != "Fresh"), aes(x=week)) + 
   geom_hline(data = hline_max2, aes(yintercept = hline_1), alpha = 0.5) +
@@ -90,13 +90,14 @@ ggplot(filter(fly_data, sample_length %in% c("Galbut_long", "Galbut_short",
   labs(x = "Time Since Sample Collection (weeks)", y = "Mean Ct" )
 
 
-
-ggsave("plots/Mean_GalbutRpL32.pdf", units = "in", width = 10, height = 8)
+# remove # to save plot
+#ggsave("plots/Mean_GalbutRpL32.pdf", units = "in", width = 10, height = 8)
 
 # get mean fold change, remove fresh samples & short samples
-fly_fc <- fly_data %>% 
+fly_fc <- fly_data3 %>% 
   group_by(target, group, week) %>% 
-  mutate(mean_fc = mean(fold_change, na.rm = TRUE),
+  mutate(fold_change = 2^-(delta_ct),
+         mean_fc = mean(fold_change, na.rm = TRUE),
          sd_fc = sd(fold_change, na.rm = TRUE),
          mean_dct = mean(delta_ct, na.rm = TRUE),
          sd_dct = sd(delta_ct, na.rm = TRUE)) %>% 
@@ -115,9 +116,10 @@ ggplot(fly_fc, aes(x = week)) +
              ncol = 2) +
   labs(x = 'Weeks After Collection', y = "Relative change", fill = "Target")
 
+# remove # based on what transformation was used in scale_y_continuous
 #ggsave("plots/Relative_fly.pdf", units = "in", width = 10, height = 8)
 #ggsave("plots/Relative_fly_log10.pdf", units = "in", width = 10, height = 8)
-ggsave("plots/Relative_fly_log2.pdf", units = "in", width = 10, height = 8)
+#ggsave("plots/Relative_fly_log2.pdf", units = "in", width = 10, height = 8)
 #ggsave("plots/Relative_fly_sqrt.pdf", units = "in", width = 10, height = 8)
 
 # delta ct fold change
@@ -135,12 +137,11 @@ ggplot(fly_fc, aes(x = week)) +
        y = "Fold Change Relative to Time Point 0 Fresh FoCo-17", 
        fill = "Target", linetype = "Group")
 
-ggsave("plots/Relative_fly_dct.pdf", units = "in", width = 10, height = 8)
-
-# Mosquito data figures
+# remove # to save plot
+#ggsave("plots/Relative_fly_dct.pdf", units = "in", width = 10, height = 8)
 
 # pre-processing 
-mosquito_data <- mosquito %>% 
+mosquito_data2 <- mosquito %>% 
   filter(present == "y") %>% 
   type_convert() %>% 
   group_by(target, group, week) %>%
@@ -149,17 +150,17 @@ mosquito_data <- mosquito %>%
          sample_group = paste0(target)) %>% 
   ungroup()
 
-# determine delta ct, use average of time 4 week as starting ct
-# calculate fold change by taking 2^-(delta_ct)
-#write_xlsx(mosquito_data, "tidy_formats/mosquito_data.xlsx")
+# determine delta ct, use average of time 4 week frozen as starting ct
+write_xlsx(mosquito_data2, "tidy_formats/mosquito_data3.xlsx")
 
 #read back in the data
-mosquito_data <- read_xlsx("tidy_formats/mosquito_data.xlsx")
+mosquito_data3 <- read_xlsx("tidy_formats/mosquito_data3.xlsx")
 
 # get mean fold change
-mosquito_data <- mosquito_data %>% 
+mosquito_data3 <- mosquito_data3 %>% 
   group_by(target, group, week) %>% 
-  mutate(mean_fc = mean(fold_change, na.rm = TRUE),
+  mutate(fold_change = 2^-(delta_ct),
+         mean_fc = mean(fold_change, na.rm = TRUE),
          sd_fc = sd(fold_change, na.rm = TRUE),
          mean_dct = mean(delta_ct, na.rm = TRUE),
          sd_dct = sd(delta_ct, na.rm = TRUE))
@@ -177,11 +178,11 @@ hline_min_mos <- data.frame(group = c("Dry", "Dry", "Dry", "Frozen", "Frozen",
                                             22.04304, 14.35967, 23.48702))
 
 # Mean line bar of all Ae. aegypti targets 
-ggplot(mosquito_data, aes(x = week)) + 
+ggplot(mosquito_data3, aes(x = week)) + 
   geom_hline(data = hline_max_mos, aes(yintercept = hline_1_mos), alpha = 0.3) +
   geom_hline(data = hline_min_mos, aes(yintercept = hline_2_mos), alpha = 0.3) +
-  geom_point(aes(y = ct, fill = target), shape=21, size=1.5, stroke=0.1, 
-             color="black", alpha = 0.75) + 
+  geom_point(aes(y = ct, fill = target), shape=21, size=1, stroke=0.1, 
+             color="black", alpha = 0.5) + 
   scale_fill_manual(values = c("turquoise3", "purple", "green")) + 
   geom_line(aes(y=mean_ct, group=sample_group)) + 
   geom_errorbar(aes(ymin = (mean_ct - sd_ct), ymax = (mean_ct + sd_ct)), 
@@ -192,26 +193,29 @@ ggplot(mosquito_data, aes(x = week)) +
   labs(x = "Time Since Sample Collection (weeks)", y = "Ct value", 
        fill = "Target")
 
-ggsave("plots/Mean_Mosquito.pdf", units = "in", width = 10, height = 8)
+# remove # to save plot
+#ggsave("plots/Mean_Mosquito.pdf", units = "in", width = 10, height = 8)
 
 # Relative change for all Ae. aegypti targets
-ggplot(mosquito_data, aes(x = week)) +
+ggplot(mosquito_data3, aes(x = week)) +
   geom_point(aes(y = fold_change, fill = group), shape = 21, size = 1, 
              stroke = 0.1, color = "black", alpha = 0.5) +
   scale_fill_manual(values = c("turquoise3", "purple")) +
   geom_line(aes(y = mean_fc, group = group, linetype = group)) +
-  scale_y_continuous(trans = "sqrt") +
+  scale_y_continuous(trans = "log2") +
   theme_few(base_size = 11) +
   facet_wrap(~factor(target, levels = c("Verdadero", "Rennavirus", "Actin")), 
-             scales = "free_y") +
+             scales = "free_y", ncol = 1) +
   labs(x = 'Weeks After Collection', y = "Relative change", fill = "Target")
 
+# remove # based on what transformation was used in scale_y_continuous
 #ggsave("plots/Relative_Mosquito_log10.pdf", units = "in", width = 10, height = 8)
 #ggsave("plots/Relative_Mosquito_log2.pdf", units = "in", width = 10, height = 8)
-ggsave("plots/Relative_Mosquito_sqrt.pdf", units = "in", width = 10, height = 8)
+#ggsave("plots/Relative_Mosquito_sqrt.pdf", units = "in", width = 10, height = 8)
+#ggsave("plots/Relative_Mosquito.pdf", units = "in", width = 10, height = 8)
 
 # delta ct fold change
-ggplot(mosquito_data, aes(x = week)) +
+ggplot(mosquito_data3, aes(x = week)) +
   geom_point(aes(y = delta_ct, fill = group), shape = 21, size = 1, 
              stroke = 0.1, color = "black", alpha = 0.5) +
   scale_fill_manual(values = c("turquoise3", "purple")) +
@@ -225,4 +229,5 @@ ggplot(mosquito_data, aes(x = week)) +
        y = "Fold Change Relative to Time Point 4 Week Mosquito", 
        fill = "Target", linetype = "Group")
 
+# remove # to save plot
 ggsave("plots/Relative_mosquito_dct.pdf", units = "in", width = 10, height = 8)
