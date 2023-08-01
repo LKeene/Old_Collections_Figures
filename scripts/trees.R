b0a6159f9946e4940159aff7cdd9ef4d10bd3fec
Lexi Keene <lexi.h.keene@gmail.com>
2023-08-01 09:50:51 -0600

    galbut RNA1 & RNA2 trees

--- scripts/trees.R
+++ scripts/trees.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggtree)
 library(treeio)
+library(gridExtra)
 
 # Practice to figure things out!
 practice <- read.beast("BEAST_trees/galbut_RNA1_practice.mcc.tre")
@@ -53,10 +54,12 @@ g1_t2 <- g1_t1 +
   theme_tree2() +
   geom_tippoint(aes(shape = Location), size = 3) +
   scale_shape_manual(values = c(0, 8, 16, 4, 17)) +
-  geom_highlight(node = 126, fill = "steelblue", alpha = 0.5) +
-  geom_highlight(node = 69, fill = "seagreen4", alpha = 0.5) +
-  geom_highlight(node = 126, fill = "steelblue", alpha = 0.5) +
-  geom_highlight(node = 68, fill = "steelblue", alpha = 0.5) +
+  geom_highlight(node = 51, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 53, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 52, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 68, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 54, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 13, fill = "deeppink", alpha = 0.5) +
   geom_range("height_0.95_HPD", color="gray50", alpha=0.2, size=1)
 g1_t2
 
@@ -65,16 +68,59 @@ rna1_metadata <- read_csv("metadata/galbut_RNA1_metadata.csv")
 rna1_metadata <- as.data.frame(rna1_metadata) 
 
 g1_t3 <- g1_t2 %<+% rna1_metadata +
-  geom_tiplab(aes(label = date, color = factor(date)), show.legend = F, 
-              hjust = -0.5, parse = T) +
-  labs(x = "Year")
+  geom_tiplab(aes(label = date, color = species), 
+              hjust = -0.4, parse = T, size = 3.5) +
+  scale_color_manual(labels = c(substitute(paste(italic("Diptera"))), 
+                                substitute(paste(italic("D. melanogaster"))), 
+                                substitute(paste(italic("D. simulans")))),
+                     values = c("seagreen", "darkblue", "orange2")) +
+  labs(x = "Year", color = "Species")
 g1_t3
 
 ggsave("plots/galbut_RNA1_tree.pdf", width = 15, height = 10)
 
 
+# Galbut RNA2 Tree
+g2 <- read.beast("BEAST_trees/Galbut_RNA2_2.mcc.tre")
 
+n_n <- ggtree(g2, mrsd="2022-01-01", color="grey30", size=0.5) +
+  geom_text(aes(label=node), hjust=-.3) +
+  geom_tippoint(aes(shape = Host))
+n_n
 
+#check to make sure labels are the same
+labels <- as.data.frame(n_n[["data"]][["label"]]) 
+
+g2_t1 <- ggtree(g2, mrsd="2022-01-01", color="grey30", size=0.5) 
+
+g2_t2 <- g2_t1 +
+  theme_tree2() +
+  geom_tippoint(aes(shape = Location), size = 3) +
+  scale_shape_manual(values = c(0, 8, 16, 4, 17)) +
+  geom_highlight(node = 60, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 57, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 58, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 56, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 59, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 12, fill = "deeppink", alpha = 0.5) +
+  geom_highlight(node = 13, fill = "deeppink", alpha = 0.5) +
+  geom_range("height_0.95_HPD", color="gray50", alpha=0.2, size=1)
+g2_t2
+
+rna2_metadata <- read_csv("metadata/galbut_RNA2_metadata.csv")
+
+rna2_metadata <- as.data.frame(rna2_metadata) 
+
+g2_t3 <- g2_t2 %<+% rna2_metadata +
+  geom_tiplab(aes(label = date, color = species), 
+              hjust = -0.4, parse = T, size = 3.5) +
+  scale_color_manual(labels = c(substitute(paste(italic("D. melanogaster"))), 
+                                substitute(paste(italic("D. simulans")))),
+                     values = c("darkblue", "orange2")) +
+  labs(x = "Year", color = "Species")
+g2_t3
+
+ggsave("plots/galbut_RNA2_tree.pdf", width = 15, height = 10)
 
 
 
