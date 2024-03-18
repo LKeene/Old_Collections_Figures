95cdfd04e48e7b3a7c620667bd15894411bfe6e1
Lexi Keene <lexi.keene@colostate.edu>
2024-03-18 11:40:50 -0600

    Updated rate figure- function of time, fixed NA

--- rates_figure.R
+++ rates_figure.R
@@ -11,8 +11,8 @@ rates2 <- rates %>%
                                           "Chaq-like virus", 
                                           "Drosophila melanogaster sigmavirus", 
                                           "Craigies Hill virus RNA 1", 
-                                          "Craigies Hill virus RNA 2", "
-                                          Dansoman virus RNA 1", "Dansoman virus RNA 2", 
+                                          "Craigies Hill virus RNA 2", 
+                                          "Dansoman virus RNA 1", "Dansoman virus RNA 2", 
                                           "Drosophila C virus", "La Jolla virus",
                                           "Nora virus", "Tobacco mosaic virus"))) %>% 
   mutate(sample_id = factor(sample_id, levels = c("California_2011_2", 
@@ -23,7 +23,7 @@ rates2 <- rates %>%
                                                   "NewJersey_1942", "Illinois_1930", 
                                                   "Minnesota_1919_3", "Illinois_1908")))
 
-rate_plot <- ggplot(rates2, aes(x = virus, y = rate)) +
+rate_plot <- ggplot(filter(rates2, percent_nucleotide_similarity >= 90), aes(x = virus, y = rate)) +
 #  scale_color_viridis() +
   geom_point(aes(color = sample_id), size = 6, alpha = 0.75) +
   scale_y_log10() +
@@ -42,3 +42,23 @@ rate_plot
 ggsave("plots/rates.pdf", units = "in", width = 10, height = 8)
 ggsave("plots/rates.svg", units = "in", width = 10, height = 8)
 ggsave("plots/rates.jpg", units = "in", width = 10, height = 8)
+
+# rate as a function of time
+rate_v_time <- ggplot(filter(rates2, percent_nucleotide_similarity >= 90), aes(x = year, y = rate)) +
+  geom_point(aes(color = virus), size = 6, alpha = 0.75) +
+  scale_y_log10() +
+  theme_minimal(base_size = 11) +
+  theme(panel.border = element_rect(linetype = "solid", fill = NA),
+        strip.background = element_rect(colour = "black", fill = "white"),
+        axis.title = element_text(face = "bold"),
+        legend.title = element_text(face = "bold"),
+        strip.text = element_text(face = "bold"),
+        #        axis.text = element_text(face = "bold"),
+        text = element_text(size = 15),
+        axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+  labs(x = "Old Collection Sequence Date (year)", y = "Rate (s/n/y)", color = "Virus Name")
+
+rate_v_time
+ggsave("plots/rate_v_time.pdf", units = "in", width = 10, height = 8)
+ggsave("plots/rate_v_time.svg", units = "in", width = 10, height = 8)
+ggsave("plots/rate_v_time.jpg", units = "in", width = 10, height = 8)
